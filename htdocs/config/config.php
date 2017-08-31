a07d794c34683775443ce1db7b91d973169d86a2
1min <dnjsxmf@naver.com>
2017-09-01 00:08:35 +0900

    2017-09-01
    web file,learning python,ruby,git file

---
+++ htdocs/config/config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+  "host"=>"localhost",
+  "duser"=>"root",
+  "dpw"=>"dhtmaks1",
+  "dname"=>"opentutorials"
+);
+?>
---
+++ htdocs/php/5.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <a href="http://poentutorials.org/course/1">생활코딩</a>
+  </body>
+</html>
