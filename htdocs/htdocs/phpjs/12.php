7bd8fd8e8c05dfd6143adfea24c9a3db4a16c99d
1min <31214658+1min@users.noreply.github.com>
2017-09-07 11:48:13 +0900

    포폴테스트추가

---
+++ htdocs/htdocs/phpjs/12.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+  <h1>JavaScript</h1>
+  <script>
+    function a() {
+      document.write("hello JS function");
+    }
+    a();
+    a();
+    a();
+    a();
+    a();
+    a();
+  </script>
+  <h1>php</h1>
+  <?php
+    function a(){
+echo "hello PHP function";
+    }
+    a();
+   ?>
+
+</body>
+</html>
