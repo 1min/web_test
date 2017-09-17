7312be05c787a9c58f04f3a368bdfd4897474a6e
1min <dnjsxmf@naver.com>
2017-09-18 01:54:34 +0900

    자바 시즌1(입출력 끝)

---
+++ java/java_tutorials/src/org/opentutorials/javatutorials/io/argstes.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.io;
+
+public class argstes {
+
+		    public static void main(String[] args){
+		        for(String e : args){
+		            System.out.println(e);
+		        }
+		    }
+		
+	}
+
+
